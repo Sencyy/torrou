c73ee705bb6f172937dcd934d195d764b24af87a
Sencyy <sencyy@protonmail.com>
2026-08-18 09:35:25 -0300

    it has a webpage now

---
+++ README.md
@@ -0,0 +1,1 @@
+![torrou](torrou.png)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/2hugame/',
+	base: '/torrou/',
 	build: {
 		assetsInclude: ['*.html', '*.css', '*.png'],
 	},
